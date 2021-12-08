18a292d4f083aa28380faeed4ec7c9a935ec3787
VilmaLundholm <92034110+VilmaLundholm@users.noreply.github.com>
2021-12-08 10:48:03 +0100

    Add files via upload

---
+++ Ica.js
@@ -0,0 +1,14 @@
+import react from "react";
+const ica=()=>{
+    const icaData=[
+        {vara:'Mjölk 3% Arla Ko', namn:'Mjölk', pris_orginal:'12,19 kr', pris_nu:'5 kr', pris_jämförelse:'5 kr/litern', antal: '10 stycken', 
+        bäst_före:'16/12-2021', inlagd:'13/12-2021', uppdaterad:'13/12- 2021  08.00'},
+        {vara:'Nötfärs 12% ca 500g Svenskt', namn:'Nötfärs', pris_orginal:'62,90 kr', pris_nu:'32 kr', pris_jämförelse:'64 kr/kg', antal: '5 stycken', 
+        bäst_före:'14/12-2021', inlagd:'12/12-2021', uppdaterad:'12/12- 2021  08.20' },
+        {vara:'Banan Eco ca 180g', namn:'Banan', pris_orginal:'22,90 kr/kg', pris_nu:'12 kr/kg', pris_jämförelse:'24 kr/kg', antal: '20 stycken', 
+        bäst_före:'15/12-2021', inlagd:'14/12-2021', uppdaterad:'14/12- 2021  08.10' },
+        {vara:'Mild yoghurt Natruell Ekologisk 3,8-4,5% 1000g Arla', namn:'Yoghurt', pris_orginal:'15,50 kr', pris_nu:'7 kr', pris_jämförelse:'7 kr/kg', antal: '3 stycken', 
+        bäst_före:'18/12-2021', inlagd:'14/12-2021', uppdaterad:'14/12- 2021  08.30' }
+    ]
+}
+export default ica
---
+++ coop.js
@@ -0,0 +1,14 @@
+import react from "react";
+const coop=()=>{
+    const coopData=[
+        {vara:'Kvarg Mild Vanilj Yalla 1000 GRM', namn:'Kvarg', pris_orginal:'35.90 kr', pris_nu:'17 kr', pris_jämförelse:'17 kr/kg', antal: '6 stycken', 
+        bäst_före:'16/12-2021', inlagd:'13/12-2021', uppdaterad:'13/12- 2021  08.00'},
+        {vara:'Äpple Royal Gala Kl 1 Polen', namn:'Äpple', pris_orginal:'6.26 kr/st', pris_nu:'2 kr/st', pris_jämförelse:'10,52 kr/kg', antal: '20 stycken', 
+        bäst_före:'14/12-2021', inlagd:'12/12-2021', uppdaterad:'12/12- 2021  07.20' },
+        {vara:'Avokado Kl 1 Peru', namn:'Avokado', pris_orginal:'12.95 kr/st', pris_nu:'8 kr/st', pris_jämförelse:'48 kr/kg', antal: '10 stycken', 
+        bäst_före:'15/12-2021', inlagd:'14/12-2021', uppdaterad:'14/12- 2021  08.10' },
+        {vara:'Kycklingbröstfilé Guldfågeln ca 900g', namn:'Kycklingbröstfilé', pris_orginal:'125.10 kr/st', pris_nu:'80.10 kr/st', pris_jämförelse:'139 kr/kg', antal: '3 stycken', 
+        bäst_före:'18/12-2021', inlagd:'14/12-2021', uppdaterad:'14/12- 2021  07.30' }
+    ]
+}
+export default coop
